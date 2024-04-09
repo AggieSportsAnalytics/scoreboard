131e20e3b3368faec998a054782774cb1158727c
vishalshenoy <vishal.nshenoy@icloud.com>
2024-04-09 10:42:53 -0700

    streamlit starter

---
+++ streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Hello Streamlit!')
+
+name = st.text_input("Enter your name", "")
+
+if st.button('Greet'):
+    st.write(f'Hello {name}, welcome to Streamlit!')
+
+# To run the app, save this script as app.py and use the following command:
+# streamlit run app.py
