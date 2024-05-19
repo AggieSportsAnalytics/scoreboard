a93455ac6804a9e55333cae93a3b5c5c25a91de1
vishalshenoy <vishal.nshenoy@icloud.com>
2024-05-18 19:48:02 -0700

    image titles

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 import streamlit as st
-from functions import parse_live_match
-from functions import parsed_player_statistics
-from functions import draymond
-from functions import shot_efficiency
-from functions import controversial_fact
-from functions import bum
-from functions import hot_hands
-from functions import shot_map
-from functions import match_odds
+from functions import parse_live_match, parsed_player_statistics, draymond, shot_efficiency, controversial_fact, bum, hot_hands, shot_map, match_odds
 import matplotlib.pyplot as plt
 import pandas as pd
 import time
 import logging
 from api import live_matches_data
 
-# layout="wide"
+# Set layout to wide
 st.set_page_config(layout="wide")
 
 
@@ -54,7 +46,7 @@ awayTeamName = fact_match['away']
 
 def display_draymond(match_id):
     dray = draymond(match_id)
-    return f"{homeTeamName}: {dray[0]} | {awayTeamName}: {dray[1]}"
+    return f"{homeTeamName}: {dray[0]}", f"{awayTeamName}: {dray[1]}"
 
 
 def generate_fact(match_id):
@@ -62,35 +54,30 @@ def generate_fact(match_id):
     return fact
 
 
-nl = '\n'
-
-
 def display_shot_efficiency(match_id):
     eff = shot_efficiency(match_id)
-    print(eff)
-    return f"{homeTeamName}: {eff[0]}% | {awayTeamName}: {eff[1]}%"
+    return f"{homeTeamName}: {str(round(eff[0], 2))}%", f"{awayTeamName}: {str(round(eff[1], 2))}%"
 
 
 def display_hot_hands(match_id):
     hot = hot_hands(match_id)
-    return f"{homeTeamName}: {hot[0]} | {awayTeamName}: {hot[1]}"
+    return f"{homeTeamName}: {hot[0]}", f"{awayTeamName}: {hot[1]}"
 
 
 def display_bum(match_id):
     bum_stat = bum(match_id)
-    return f"{homeTeamName}: {bum_stat[0]} | {awayTeamName}: {bum_stat[1]}"
+    return f"{homeTeamName}: {bum_stat[0]}", f"{awayTeamName}: {bum_stat[1]}"
 
 
 def display_match_odds(match_id):
     mo = match_odds(match_id)
-    return f"{homeTeamName}: {str(round(mo[0], 2))}% | {awayTeamName}: {str(round(mo[1], 2))}%"
+    return f"{homeTeamName}: {str(round(mo[0], 2))}%", f"{awayTeamName}: {str(round(mo[1], 2))}%"
 
 
 def run_app(event_number):
     stat_title = st.empty()
     placeholder_home_player_statistics = st.empty()
     placeholder_away_player_statistics = st.empty()
-
     placeholder_shot_map = st.empty()
     court_img = plt.imread('./images/shot_chart.webp')
     fig, ax = plt.subplots()
@@ -101,10 +88,15 @@ def run_app(event_number):
     next = 0
 
     placeholder_draymond = st.empty()
+    placeholder_draymond_2 = st.empty()
     placeholder_hot_hands = st.empty()
+    placeholder_hot_hands_2 = st.empty()
     placeholder_bum = st.empty()
+    placeholder_bum_2 = st.empty()
     placeholder_shot_efficiency = st.empty()
+    placeholder_shot_efficiency_2 = st.empty()
     placeholder_match_odds = st.empty()
+    placeholder_match_odds_2 = st.empty()
     placeholder_fact = st.empty()
 
     global homeTeamName
@@ -114,13 +106,19 @@ def run_app(event_number):
     fact_match_id = fact_match.pop('id')
     homeTeamName = fact_match['home']
     awayTeamName = fact_match['away']
-    print(homeTeamName)
-    print(awayTeamName)
     fact = generate_fact(fact_match_id)
-    print("fact generated")
-    print(fact)
 
-    # max one minute so that it doesn't accidentally run in the background
+    image_map = {
+        -1: "./images/text.png",
+        0: "./images/text.png",
+        1: "./images/text.png",
+        2: "./images/text.png",
+        3: "./images/text.png",
+        4: "./images/text.png",
+        5: "./images/text.png",
+        6: "./images/text.png"
+    }
+
     for seconds in range(30):
         match = parse_live_match(0)
         match_id = match.pop('id')
@@ -134,90 +132,75 @@ def run_app(event_number):
         placeholder_home_player_statistics.empty()
         placeholder_shot_map.empty()
         placeholder_draymond.empty()
+        placeholder_draymond_2.empty()
         placeholder_shot_efficiency.empty()
+        placeholder_shot_efficiency_2.empty()
         placeholder_hot_hands.empty()
+        placeholder_hot_hands_2.empty()
         placeholder_bum.empty()
+        placeholder_bum_2.empty()
         placeholder_fact.empty()
         placeholder_match_odds.empty()
+        placeholder_match_odds_2.empty()
 
-        if switch == -1:  # next needs to be initialized to a base value
-            next = display_player_statistics(
-                match_id)
+        if switch == -1:  # General Statistics
+            next = display_shot_map(
+                match_id, home_team_id, away_team_id, fig, ax)
             switch = 0
-        elif switch == 0:
-            switch += 1
-            # general
-            stat_title.title("Game Statistics")
-            placeholder_home_player_statistics.table(next[0])
-            placeholder_away_player_statistics.table(next[1])
-            next = display_shot_map(match_id, home_team_id,
-                                    away_team_id, fig, ax)
-            # placeholder_away_player_statistics.title("1")
-            time.sleep(2)
-        elif switch == 1:
-            switch += 1
-            # shotmap
-            stat_title.title("Player Shotmap")
+        elif switch == 0:  # Player Shotmap
+            stat_title.image(image_map[switch])
             placeholder_shot_map.pyplot(next)
-            next = display_draymond(
-                match_id)
-            # placeholder_shot_map.title("2")
+            next = display_draymond(match_id)
             time.sleep(2)
-        elif switch == 2:
             switch += 1
-            # draymond
-            stat_title.title("Draymond/Foul")
-            placeholder_draymond.title(next)
-            st.image('./images/peter.png')
-            next = display_hot_hands(
-                match_id)
+        elif switch == 1:  # Draymond/Foul
+            stat_title.image(image_map[switch])
+            placeholder_draymond.title(next[0])
+            placeholder_draymond_2.title(next[1])
+            next = display_hot_hands(match_id)
             time.sleep(2)
-        elif switch == 3:
             switch += 1
-            # hot hands
-            stat_title.title("Hot Hands")
-            placeholder_hot_hands.title(next)
-            next = display_bum(
-                match_id)
+        elif switch == 2:  # Hot Hands
+            stat_title.image(image_map[switch])
+            placeholder_hot_hands.title(next[0])
+            placeholder_hot_hands_2.title(next[1])
+            next = display_bum(match_id)
             time.sleep(2)
-        elif switch == 4:
             switch += 1
-            # bum
-            stat_title.title("Bum")
-            placeholder_bum.title(next)
-            next = display_shot_efficiency(
-                match_id)
+        elif switch == 3:  # Bum
+            stat_title.image(image_map[switch])
+            placeholder_bum.title(next[0])
+            placeholder_bum_2.title(next[1])
+            next = display_shot_efficiency(match_id)
             time.sleep(2)
-        elif switch == 5:
             switch += 1
-            # shot efficiency
-            stat_title.title("Shot Efficiency")
-            placeholder_shot_efficiency.title(next)
-            next = display_match_odds(
-                match_id)
+        elif switch == 4:  # Shot Efficiency
+            stat_title.image(image_map[switch])
+            placeholder_shot_efficiency.title(next[0])
+            placeholder_shot_efficiency_2.title(next[1])
+            next = display_match_odds(match_id)
             time.sleep(2)
-        elif switch == 6:
             switch += 1
-            # match odds
-            stat_title.title("Match Odds")
-            placeholder_match_odds.title(next)
+        elif switch == 5:  # Match Odds
+            stat_title.image(image_map[switch])
+            placeholder_match_odds.title(next[0])
+            placeholder_match_odds_2.title(next[1])
             next = fact
             time.sleep(2)
-        elif switch == 7:
             switch += 1
-            # fun fact
-            stat_title.title("Fun Fact")
+        elif switch == 6:  # Fun Fact
+            stat_title.image(image_map[switch])
             placeholder_fact.title(next)
             next = 1
             time.sleep(2)
-        elif switch == 8:
+            switch += 1
+        elif switch == 7:
             switch = -1
 
 
 data = live_matches_data()
 placeholders = []
 buttons = []
-
 
 if len(data['events']) == 0:
     st.title("No Games Currently")
@@ -239,5 +222,4 @@ else:
             for placeholder in placeholders:
                 placeholder.empty()
             run_app(index)
-
             break
